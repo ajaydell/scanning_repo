9ef7a5c7a7f9cc4500d171c1dc4e0bf8347f32d1
ajaydell <33676904+ajaydell@users.noreply.github.com>
2020-05-29 17:16:20 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -1,8 +1,8 @@
-DB_Patterns=r"    patterns = (dapi[A-Za-z0-9]|dapi)[A-Za-z0-9]{32}"
-SQL_Patterns=r"    patterns = (\"|')?(SQL|sql|Sql)?_?(ADMIN|admin|Admin)?_?(CREDENTIAL|credential|Credential)?_?(PWD|pwd|Pwd)?_?(PASSWORD|password|Password)(\"|')?\\s*(:|=>|=)\\s*(\"|')?\\s*.+{10,15}(\"|')?"
-Storage_Blob_Patterns=r"    patterns = (\"|')?(STORAGE|storage|Storage)?_?(ACCOUNT|account|Account)?_?(ACCESS|access|Access)?_?(KEY|key|Key)(\"|')?\\s*(:|=>|=)\\s*(\"|')?[A-zA-Za-z0-9+/=!@$%&*?"#"\\^+=]{88}(\"|')?
-GIT_Pat_Patterns=r"    patterns = (\"|')?(GIT|git|Git)?_?(PAT|pat|Pat)?_?(TOKEN|token|Token)?_?(SECRET|secret|Secret)(\"|')?\\s*(:|=>|=)\\s*(\"|')?[A-zA-Za-z0-9!@$%&*?"#"\\^+=]{40}(\"|')?
-SQL_Pwd_patterns=r"    patterns = (\"|')?(SQL|sql|Sql)?_?(ADMIN|admin|Admin)?_?(CREDENTIAL|credential|Credential)?_?(PWD|pwd|Pwd)(\"|')?\\s*(:|=>|=)\\s*(\"|')?[A-zA-Za-z0-9!@$%&*?"#"\\^=]{10,15}(\"|')?
+DB_Patterns=r"   patterns = (dapi[A-Za-z0-9]|dapi)[A-Za-z0-9]{32}"
+SQL_Patterns=r"   patterns = (\"|')?(SQL|sql|Sql)?_?(ADMIN|admin|Admin)?_?(CREDENTIAL|credential|Credential)?_?(PWD|pwd|Pwd)?_?(PASSWORD|password|Password)(\"|')?\\s*(:|=>|=)\\s*(\"|')?\\s*.+{10,15}(\"|')?"
+Storage_Blob_Patterns=r"   patterns = (\"|')?(STORAGE|storage|Storage)?_?(ACCOUNT|account|Account)?_?(ACCESS|access|Access)?_?(KEY|key|Key)(\"|')?\\s*(:|=>|=)\\s*(\"|')?[A-zA-Za-z0-9+/=!@$%&*?"#"\\^+=]{88}(\"|')?
+GIT_Pat_Patterns=r"   patterns = (\"|')?(GIT|git|Git)?_?(PAT|pat|Pat)?_?(TOKEN|token|Token)?_?(SECRET|secret|Secret)(\"|')?\\s*(:|=>|=)\\s*(\"|')?[A-zA-Za-z0-9!@$%&*?"#"\\^+=]{40}(\"|')?
+SQL_Pwd_patterns=r"   patterns = (\"|')?(SQL|sql|Sql)?_?(ADMIN|admin|Admin)?_?(CREDENTIAL|credential|Credential)?_?(PWD|pwd|Pwd)(\"|')?\\s*(:|=>|=)\\s*(\"|')?[A-zA-Za-z0-9!@$%&*?"#"\\^=]{10,15}(\"|')?
 
 path = 'D:/a/1/s/.git/config'
 
